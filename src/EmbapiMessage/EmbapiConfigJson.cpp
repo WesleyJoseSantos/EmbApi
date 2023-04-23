cc8f2e6c308dd6dce4629ba6502f13ea1b85ffc7
Wesley José Santos <binary_quantum@outlook.com>
2023-04-23 05:19:35 -0300

    feat: implement EmbapiConfigJson

--- src/EmbapiMessage/EmbapiConfigJson.cpp
+++ src/EmbapiMessage/EmbapiConfigJson.cpp
@@ -9,86 +9,68 @@
  * 
  */
 
+#include "EmbapiCore/EmbapiError.h"
 #include "EmbapiMessage/EmbapiConfigJson.h"
-#include "ArduinoJson.h"
+#include "EmbapiMessage/WifiConfigJson.h"
+#include "EmbapiMessage/NtpConfigJson.h"
+#include "EmbapiMessage/MqttConfigJson.h"
 
-#define JSON_DOC_SIZE (sizeof(EmbapiConfigJson)*2)
-#define JSON_CHAR_SIZE (sizeof(EmbapiConfigJson)*2)
-
-static StaticJsonDocument<JSON_DOC_SIZE> doc;
-static char json[JSON_CHAR_SIZE];
-
-char *EmbapiConfigJson::toJson()
+void EmbapiConfigJson::toJson(JsonDocument *doc)
 {
-    EmbapiMessageType id = (EmbapiMessageType)this->id;
-
-    doc["id"] = id;
+    (*doc)["id"] = this->id;
 
     switch (id)
     {
-    case EMBAPI_CONFIG_ID_UNKNOWN:
-        doc["error"] = EmbapiError::UNKNOWN_ID;
-        break;
-    case EMBAPI_CONFIG_ID_WIFI:
-        doc["ssid"] = this->config.wifiConfig.getSSID();
-        doc["password"] = this->config.wifiConfig.getPassword();
-        break;
-    case EMBAPI_CONFIG_ID_ETHERNET:
-        doc["error"] = EmbapiError::NOT_IMPLEMENTED;
-        break;
-    case EMBAPI_CONFIG_ID_SERIAL:
-        doc["error"] = EmbapiError::NOT_IMPLEMENTED;
-        break;
-    case EMBAPI_CONFIG_ID_BLUETOOTH:
-        doc["error"] = EmbapiError::NOT_IMPLEMENTED;
-        break;
-    case EMBAPI_CONFIG_ID_NTP:
-        doc["interval"] = this->config.ntp.getInterval();
-        doc["server"] = this->config.ntp.getServer();
-        break;
-    case EMBAPI_CONFIG_ID_MQTT:
-        doc["url"] = this->config.mqtt.getUrl();
-        doc["port"] = this->config.mqtt.getPort();
-        doc["user"] = this->config.mqtt.getUser();
-        doc["password"] = this->config.mqtt.getPassword();
-        break;
+        case EMBAPI_CONFIG_ID_UNKNOWN:
+            this->error = EmbapiError::UNKNOWN_ID;
+            break;
+        case EMBAPI_CONFIG_ID_WIFI:
+            WifiConfigJson(this->config.wifi).toJson(doc);
+            break;
+        case EMBAPI_CONFIG_ID_ETHERNET:
+            this->error = EmbapiError::NOT_IMPLEMENTED;
+            break;
+        case EMBAPI_CONFIG_ID_SERIAL:
+            this->error = EmbapiError::NOT_IMPLEMENTED;
+            break;
+        case EMBAPI_CONFIG_ID_BLUETOOTH:
+            this->error = EmbapiError::NOT_IMPLEMENTED;
+            break;
+        case EMBAPI_CONFIG_ID_NTP:
+            NtpConfigJson(this->config.ntp).toJson(doc);
+            break;
+        case EMBAPI_CONFIG_ID_MQTT:
+            MqttConfigJson(this->config.mqtt).toJson(doc);
+            break;
     }
-
-    serializeJson(doc, json);
-    return json;
 }
 
-EmbapiError EmbapiConfigJson::fromJson(char *json)
+void EmbapiConfigJson::fromJson(JsonDocument *doc)
 {
-    deserializeJson(doc, json);
-
-    EmbapiConfigId id = (EmbapiConfigId)doc["id"].as<uint8_t>();
+    this->id = (*doc)["id"];
 
     switch (id)
     {
-    case EMBAPI_CONFIG_ID_UNKNOWN:
-        return EmbapiError::UNKNOWN_ID;
-    case EMBAPI_CONFIG_ID_WIFI:
-        this->config.wifiConfig.setSSID(doc["ssid"]);
-        this->config.wifiConfig.setPassword(doc["password"]);
-        return EmbapiError::NO_ERROR;
-    case EMBAPI_CONFIG_ID_ETHERNET:
-        return EmbapiError::NOT_IMPLEMENTED;
-    case EMBAPI_CONFIG_ID_SERIAL:
-        return EmbapiError::NOT_IMPLEMENTED;
-    case EMBAPI_CONFIG_ID_BLUETOOTH:
-        return EmbapiError::NOT_IMPLEMENTED;
-    case EMBAPI_CONFIG_ID_NTP:
-        this->config.ntp.setInterval(doc["interval"]);
-        this->config.ntp.setServer(doc["server"]);
-        return EmbapiError::NO_ERROR;
-    case EMBAPI_CONFIG_ID_MQTT:
-        this->config.mqtt.setUrl(doc["url"]);
-        this->config.mqtt.setPort(doc["port"]);
-        this->config.mqtt.setUser(doc["user"]);
-        this->config.mqtt.setPassword(doc["password"]);
-        return EmbapiError::NO_ERROR;
+        case EMBAPI_CONFIG_ID_UNKNOWN:
+            (*doc)["error"] = EmbapiError::UNKNOWN_ID;
+            break;
+        case EMBAPI_CONFIG_ID_WIFI:
+            this->config.wifi = WifiConfigJson(doc);
+            break;
+        case EMBAPI_CONFIG_ID_ETHERNET:
+            (*doc)["error"] = EmbapiError::NOT_IMPLEMENTED;    
+            break;
+        case EMBAPI_CONFIG_ID_SERIAL:
+            (*doc)["error"] = EmbapiError::NOT_IMPLEMENTED;    
+            break;
+        case EMBAPI_CONFIG_ID_BLUETOOTH:
+            (*doc)["error"] = EmbapiError::NOT_IMPLEMENTED;    
+            break;
+        case EMBAPI_CONFIG_ID_NTP:
+            this->config.ntp = NtpConfigJson(doc);
+            break;
+        case EMBAPI_CONFIG_ID_MQTT:
+            this->config.mqtt = MqttConfigJson(doc);
+            break;
     }
-
-    return EmbapiError::NO_ERROR;
 }
