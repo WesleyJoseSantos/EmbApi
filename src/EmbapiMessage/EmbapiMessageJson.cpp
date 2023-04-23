12ef84e2437d434a32851ae15c60673e1c106cdb
Wesley José Santos <binary_quantum@outlook.com>
2023-04-23 06:51:17 -0300

    feat: implement EmbapiMessageJson

--- src/EmbapiMessage/EmbapiMessageJson.cpp
+++ src/EmbapiMessage/EmbapiMessageJson.cpp
@@ -10,3 +10,33 @@
  */
 
 #include "EmbapiMessage/EmbapiMessageJson.h"
+#include "EmbapiMessage/EmbapiConfigJson.h"
+
+void EmbapiMessageJson::toJson(JsonDocument *doc) {
+    (*doc)["type"] = this->type;
+
+    switch (type)
+    {
+        case EMBAPI_MESSAGE_TYPE_UNKNOWN:
+            this->error = (*doc)["error"];
+            break;
+        case EMBAPI_MESSAGE_TYPE_CONFIG:
+            EmbapiConfigJson(this->data.config).toJson(doc);
+            break;
+    }
+}
+
+void EmbapiMessageJson::fromJson(JsonDocument *doc) {
+    this->type = (*doc)["type"];
+    this->error = (*doc)["error"];
+
+    switch (type)
+    {
+        case EMBAPI_MESSAGE_TYPE_UNKNOWN:
+            this->error = EmbapiError::UNKNOWN_TYPE;
+            break;
+        case EMBAPI_MESSAGE_TYPE_CONFIG:
+            this->data.config = EmbapiConfigJson(doc);
+            break;
+    }
+}
