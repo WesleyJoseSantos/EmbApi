a5bf9906bdf34dc3575203c15949ac0ff2655e7f
Wesley José Santos <binary_quantum@outlook.com>
2023-04-25 19:06:35 -0300

    feat: implement tests

---
+++ test/test_EmbapiMessage/MqttConfigJsonTests.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file MqttConfigJsonTests.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-23
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "unity.h"
+#include "mocks/MqttConfigJsonMock.h"
+#include "EmbapiMessage/EmbapiConfigJson.h"
+
+void setUp(void) {
+  // set stuff up here
+}
+
+void tearDown(void) {
+  // clean stuff up here
+}
+
+int runUnityTests(void) {
+  UNITY_BEGIN();
+  return UNITY_END();
+}
+
+int main(void) {
+  return runUnityTests();
+}
